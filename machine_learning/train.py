9e10040847ecac28f8ab199590f53301dcb50a11
brandoluu <brandonluu9@gmail.com>
2026-01-13 23:58:36 -0800

    added grid search training

---
+++ machine_learning/train.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import plot_tree
+from category_encoders import TargetEncoder
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+
+
+
+def generate_tree(model, df, output_path='tree.png'):
+    """
+    Generates a visualization of the first tree in the RandomForest model.
+    
+    Args:
+        model: Trained RandomForest model
+        df: Pandas DataFrame with feature columns (used for feature names)
+        output_path: Path to save the tree image
+    """
+    feature_names = df.columns.tolist()
+    tree = model.estimators_[0]  # Visualize the first tree in the forest
+    
+    plt.figure(figsize=(20, 10))
+    plot_tree(tree, feature_names=feature_names, filled=True, rounded=True, max_depth=3)
+    plt.savefig(output_path)
+    print(f"Tree visualization saved to {output_path}")
+    plt.close()
+
+
+
+def train_model(data_path):
+    df = pd.read_csv(data_path)
+
+    # Encoding name features with target encoding
+    nameEncoder = TargetEncoder(cols=['sire', 'dam', 'bmSire'], smoothing=10.0)
+
+    y = df['rating']
+    X = df.drop(columns=['rating', 'name'])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+    X_train = nameEncoder.fit_transform(X_train, y_train)  
+    X_test = nameEncoder.transform(X_test)                 
+
+    paramGrid = {
+        'n_estimators': [100, 200, 300],
+        'max_depth': [None, 10, 20],
+        'max_features': ['auto', 'sqrt', 'log2'],
+        'min_samples_split': [2, 5],
+        'min_samples_leaf': [1, 2],
+        'criterion': ['squared_error', 'absolute_error', 'gini', 'entropy']
+    }
+
+
+    rf = RandomForestRegressor(n_estimators=100, random_state=42, verbose=1)
+
+    gridSearch = GridSearchCV(estimator=rf, param_grid=paramGrid, cv=5, n_jobs=-1, verbose=2)
+
+    # Fitting the model
+    gridSearch.fit(X_train, y_train)
+
+    # Generate tree visualization
+    generate_tree(rf, X_train)
+
+    # Evaluating the model with predctions on X_test
+
+    y_pred = gridSearch.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+
+    r2 = r2_score(y_test, y_pred)
+    accuracy = gridSearch.score(X_test, y_test)
+
+    print(f"mse: {mse}")
+    print(f"r2: {r2}")
+    print(f"accuracy: {accuracy}")
+
+
+
+if __name__ == "__main__":
+    data_path = "data/baseData.csv"
+    train_model(data_path)
